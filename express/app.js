9e3b8b93c9e29c9e31330f06060de4f1188448c3
johnpauljpc <jpcwork081@gmail.com>
2023-04-21 19:57:28 +0100

    routing and html pages in express

--- express/app.js
+++ express/app.js
@@ -5,5 +5,14 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) =>{
+    // res.send('<h1> Hello Guys, I love Express</h1>')
     res.sendFile('./views/index.html', {root: __dirname})
+});
+
+app.get('/about', (req, res)=>{
+    res.sendFile('./views/about-us.html', {root:__dirname})
+})
+
+app.get('/contact', (req, res)=>{
+    res.sendFile('./views/contact-page.html', {root:__dirname})
 })
